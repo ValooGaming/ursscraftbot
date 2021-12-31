fc9b586a9d8b416bb54f9f41416ab88215a574df
Valentin <lesrageursdu13@gmail.com>
2021-12-31 01:55:58 +0100

    djs12 update

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = (client, member) => {
     member.guild.channels.cache.get('901159530199666750').send(':smile: **Bienvenue **' + member.user + " :partying_face: ** Nous sommes **" + member.guild.memberCount);
-    member.addRole('901160471053357128')
+    member.roles.add('901160471053357128')
 }
