8740df1f922681d8bcea8a379345513eb3be8c91
Valentin <lesrageursdu13@gmail.com>
2022-01-11 10:50:38 +0100

    guildMemberAdd and Remove patch

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,5 @@
 module.exports = (client, member) => {
-    member.guild.channels.cache.get('901159530199666750').send(':smile: **Bienvenue **' + member.user + " :partying_face: ** Nous sommes **" + member.guild.memberCount);
+    console.log(member)
+    member.guild.channels.cache.get('901159530199666750').send(':smile: **Bienvenue **' + "<@"+member.user+">" + " :partying_face: ** Nous sommes **" + member.guild.memberCount);
     member.roles.add('901160471053357128')
 }
