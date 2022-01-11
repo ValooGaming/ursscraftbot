7b9cb18c403586f20af28d0215321263fe3048dc
Valentin <lesrageursdu13@gmail.com>
2022-01-11 11:00:00 +0100

    message when bot is up

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 module.exports = async(client, data) => {
     console.log("bot lancé")
     client.user.setActivity("ursscraft.mine.fun")
+    client.channels.cache.get(process.env.STATUS_CHANNEL_ID).send("🟢 Bot allumé !")
 }
